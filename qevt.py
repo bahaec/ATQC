7c16248e7543bc0d34d2c10adf9832d36d77ae8a
go98maw <bahaeddine.chikhi@tum.de>
2025-06-04 11:40:22 +0200

    -

--- qevt.py
+++ qevt.py
@@ -3,16 +3,16 @@ from matplotlib import pyplot as plt
 import tkinter as tk
 
 PI = np.pi
-    
+
+def test():
+    return
+
 x = []
 y1 = []
 y2 = []
 
 root = tk.Tk()
-root.title("QSP Simulation")
-tk.Label(root, text="Choose a QSP sequence to simulate:", font=("Arial", 14)).pack(pady=10)
-tk.Button(root, text="BB1 Sequence", command=bb1plot, width=25).pack(pady=5)
-tk.Button(root, text="1st Chebyshev Polynomial", command=chebychev_1plot, width=25).pack(pady=5)
-tk.Button(root, text="2nd Chebyshev Polynomial", command=chebychev_2plot, width=25).pack(pady=5)
-tk.Button(root, text="3rd Chebyshev Polynomial", command=chebychev_3plot, width=25).pack(pady=5)
+root.title("QEVT Simulation")
+tk.Label(root, text="Choose a QEVT simulation:", font=("Arial", 14)).pack(pady=10)
+tk.Button(root, text="test", command=test, width=25).pack(pady=5)
 root.mainloop()
